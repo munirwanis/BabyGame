8946517e47492b2ca9d1fa0c40f0536d46a7a73c
LostLobos <igormorse@hotmail.com>
2015-02-02 18:44:45 -0200

    Versão Inicial. Primeiro Commit.
    Atualização: Script para agarrar o cubo na Mão.

---
+++ Assets/Scripts/MouseEvents.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseEvents : MonoBehaviour {
+
+    public float OriginalPosZ = 15.13532f;
+    public float Depth = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float posz = this.transform.position.z;
+
+        Vector2 MousePos = Input.mousePosition;
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (posz != OriginalPosZ)
+                posz = OriginalPosZ;
+            else
+                posz = 20.13531f;
+        }
+
+        Vector3 HandPosition = Camera.main.ScreenToWorldPoint(new Vector3(MousePos.x, MousePos.y, Depth));
+
+        HandPosition.z = posz;
+
+        transform.position = HandPosition;  
+	}
+
+    void OnCollisionEnter(Collision collision){
+
+		if (collision.gameObject.name == "Cubo")
+			Debug.Log("HUEHUEHUE:");
+
+	}
+}
---
+++ Assets/Scripts/ObjectEvent.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectEvent : MonoBehaviour {
+
+    public bool Colisao = false;
+    public GameObject Mestre;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Colisao)
+        {
+            this.transform.position = Mestre.transform.position;
+        }
+	
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "babyhand")
+        {
+            Debug.Log("COLISAO CUBO!");
+            Mestre = collision.gameObject;
+            Colisao = true;
+        }
+
+    }
+}
