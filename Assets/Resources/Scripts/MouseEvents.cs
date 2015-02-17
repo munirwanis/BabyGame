89b2b88c36ce769abd8ad9c49d987ed345b0eff4
LostLobos <igormorse@hotmail.com>
2015-02-17 11:52:50 -0200

    Atualização

---
+++ Assets/Resources/Scripts/MouseEvents.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseEvents : MonoBehaviour {
+
+    public float HandSpeed = 0.1f;
+    public float Depth = 15f;
+    public bool ObjectOnHand = false;
+    public bool Collision = false;
+    public GameObject Cubo;
+    public GameObject Triangulo;
+    public GameObject Cilindro;
+    public GameObject Objeto = null;
+
+	// Use this for initialization
+	void Start () {
+    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (Input.GetMouseButton(0) && Collision == false)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+                Depth += HandSpeed * 5;
+            else
+                Depth += HandSpeed;
+        }
+        if (Input.GetMouseButton(1) && Collision == false)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+                Depth -= HandSpeed * 5;
+            else
+                Depth -= HandSpeed;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Alpha1) && ObjectOnHand == false)
+        {
+            Objeto = Instantiate(Cubo, transform.position, Quaternion.Euler(new Vector3(330.8729f,3.809764f,358.1435f))) as GameObject;
+            Objeto.transform.localScale = Cubo.transform.localScale * 200;
+        }
+
+        if (ObjectOnHand)
+            renderer.enabled = false;
+        else
+            renderer.enabled = true;
+
+        Ray HandRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Vector3 HandPosition = HandRay.GetPoint(Depth);
+
+        transform.position = HandPosition;
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Cubo" || collision.gameObject.name == "Triangulo" || collision.gameObject.name == "Cilindro")
+            if (collision.gameObject.GetComponent<ObjectEvent>().OnTable == true && ObjectOnHand == false)
+                Collision = true;
+
+        if (collision.gameObject.name.Contains("Cubo") == false && collision.gameObject.name.Contains("Triangulo") == false && collision.gameObject.name.Contains("Cilindro") == false)
+            Collision = true;
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        Collision = false;
+    }
+
+}
---
+++ Assets/Resources/Scripts/ObjectEvent.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectEvent : MonoBehaviour {
+
+    public float InitialLocalPosX;
+    public float InitialLocalPosZ;
+    public float FinalLocalPosY = 0.013f;
+    public bool OnTable = false;
+    public bool Grab = false;
+    public GameObject Mestre;
+
+	// Use this for initialization
+	void Start () {
+
+        Vector3 MyPosition = transform.position;
+
+        if (this.gameObject.name != "Mesa")
+            renderer.material.color = Color.blue;
+
+        InitialLocalPosX = transform.localPosition.x;
+        InitialLocalPosZ = transform.localPosition.z;
+
+        transform.position = transform.position * 4;
+
+        RandomPosition(MyPosition);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyUp(KeyCode.Space) && Mestre != null)
+        {
+            Mestre.GetComponent<MouseEvents>().ObjectOnHand = false;
+            Grab = false;
+        }     
+
+        if (Grab)
+            this.transform.position = Mestre.transform.position;
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == ("Colisor " + gameObject.name))
+        {
+            transform.localPosition = new Vector3(InitialLocalPosX, FinalLocalPosY, InitialLocalPosZ);
+            renderer.material.color = Color.green;
+            OnTable = true;
+            Grab = false;
+            Mestre.GetComponent<MouseEvents>().ObjectOnHand = false;
+        }
+        else if (collision.gameObject.name.Contains("Colisor"))
+            renderer.material.color = Color.red;
+       
+        if (collision.gameObject.name == "babyhand" && OnTable == false)
+        {
+            if (collision.gameObject.GetComponent<MouseEvents>().ObjectOnHand == false)
+            {
+                Mestre = collision.gameObject;
+                Mestre.GetComponent<MouseEvents>().ObjectOnHand = true;
+                Grab = true;
+            }
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (renderer.material.color == Color.red)
+            renderer.material.color = Color.blue;
+    }
+
+    void RandomPosition(Vector3 MyPosition)
+    {
+        float[] Positions = {-3.644695f, -0.3142252f, 3.494558f};
+        bool Ocupado = true;
+        int AntiLoop = 0;
+        int Rnd;
+        Ray RayCast;
+        RaycastHit Hit;
+
+        while (Ocupado && AntiLoop < 20)
+        {
+            Rnd = Random.Range(0, 3);
+            RayCast = new Ray(Vector3.zero, new Vector3(Positions[Rnd], MyPosition.y, MyPosition.z));
+
+            if (Rnd == 0)
+                Debug.DrawRay(Vector3.zero, new Vector3(Positions[Rnd], MyPosition.y, MyPosition.z), Color.red, 10f);
+            else if (Rnd == 1)
+                Debug.DrawRay(Vector3.zero, new Vector3(Positions[Rnd], MyPosition.y, MyPosition.z), Color.green, 10f);
+            else
+                Debug.DrawRay(Vector3.zero, new Vector3(Positions[Rnd], MyPosition.y, MyPosition.z), Color.blue, 10f);
+
+            if (Physics.Raycast(RayCast, out Hit))
+            {
+                if (Hit.collider == null)
+                {
+                    Debug.Log(Hit.collider.gameObject.name);
+                    transform.position = new Vector3(Positions[Rnd], MyPosition.y, MyPosition.z);
+                    Ocupado = false;
+                }
+                else
+                {
+                    AntiLoop++;
+                    if (Hit.collider.gameObject.name == "Cubo")
+                    {
+                        Debug.Log("COLLIDER POS: " + Hit.transform.position);
+                        Debug.Log("POSICAO ATUAL: " + transform.position);
+                        Debug.Log("POSICAO ANTIGA: " + MyPosition);
+                    }
+                }
+            }
+                
+        }
+    }
+}
